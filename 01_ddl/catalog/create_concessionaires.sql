416fb0168d58dd655840f88c300ea3a61610020e
carlos-aguirre056 <carlosaguirreo056@gmail.com>
2025-12-10 16:47:58 -0600

    Removal of 4 columns of concessionaires
    It was determined that the coulmns registering addressing data of the concessionaires should belong in a child table, not in a master table.

--- 01_ddl/catalog/create_concessionaires.sql
+++ 01_ddl/catalog/create_concessionaires.sql
@@ -10,12 +10,6 @@ for registry, operational, and historical modules.
 CREATE TABLE catalog.concessionaires(
 id_concessionaire INT IDENTITY(1,1),
 full_name VARCHAR(100) NOT NULL,
-street VARCHAR(100) NOT NULL,
-street_number SMALLINT NOT NULL,
-neighborhood VARCHAR(100) NOT NULL,
-postal_code VARCHAR(10) NOT NULL,
-town VARCHAR(50) NOT NULL,
-municipality VARCHAR(50) NOT NULL,
 rfc CHAR(13) UNIQUE, --It is CHAR 13 cause all concesionaires are "personas fisicas"
 curp CHAR(18) UNIQUE NOT NULL,
 
