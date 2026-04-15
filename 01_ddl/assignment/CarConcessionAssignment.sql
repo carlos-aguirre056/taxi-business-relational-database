fe8879dc30253510b09ec26b6d611a0b1a4f4aa8
carlos-aguirre056 <carlosaguirreo056@gmail.com>
2026-04-15 17:36:52 -0600

    Create CarConcessionAssignment table with constraints

---
+++ 01_ddl/assignment/CarConcessionAssignment.sql
@@ -0,0 +1,13 @@
+--RELATIONSHIP ATTRIBUTE
+  -- 1.2.3 CAR_CONCESSION_ASSIGNMENT
+CREATE TABLE CarConcessionAssignment(
+  id_car_concession_history INT IDENTITY
+, id_car INT
+, id_concession INT
+, start_date DATE NOT NULL
+, end_date DATE NULL
+
+CONSTRAINT pk_car_concession_assignment PRIMARY KEY(Id_car_concession_history)
+, CONSTRAINT fk_car_concession_assignment_id_car FOREIGN KEY (id_car) REFERENCES catalog.car(id_car)
+  GO
+  );
