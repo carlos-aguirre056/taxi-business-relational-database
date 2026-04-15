34edd0cd6ce90e62d4943171bac9ed1941f3275c
carlos-aguirre056 <carlosaguirreo056@gmail.com>
2026-04-15 17:29:39 -0600

    Create CarDriverAssignment table with constraints

---
+++ 01_ddl/assignment/CarDriverAssignment.sql
@@ -0,0 +1,14 @@
+--RELATIONSHIP ENTITY
+-- 1.2.2 CAR DRIVER ASSGINMENT
+CREATE TABLE CarDriverAssignment(
+    id_car_driver_history INT IDENTITY
+    , id_person INT NOT NULL
+    , id_car INT NOT NULL
+    , start_date DATE NOT NULL
+    , end_date DATE NULL
+
+CONSTRAINT pk_CarDriverAssignment PRIMARY KEY(id_car_driver_history)
+, CONSTRAINT fk_CarDriverAssignment_person_id_driver FOREIGN KEY(id_person)
+, CONSTRAINT fk_CarDriverAssignment_car_id FOREIGN KEY(id_car)
+
+GO);
