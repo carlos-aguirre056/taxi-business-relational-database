a71da01a01226576afe98f997771471ddccc424c
carlos-aguirre056 <carlosaguirreo056@gmail.com>
2026-04-16 15:29:35 -0600

    Add CHECK constraint for valid date range

--- 01_ddl/assignment/CarConcessionAssignment.sql
+++ 01_ddl/assignment/CarConcessionAssignment.sql
@@ -7,7 +7,11 @@ CREATE TABLE CarConcessionAssignment(
 , start_date DATE NOT NULL
 , end_date DATE NULL
 
+  --PK and FK
 CONSTRAINT pk_car_concession_assignment PRIMARY KEY(Id_car_concession_history)
 , CONSTRAINT fk_car_concession_assignment_id_car FOREIGN KEY (id_car) REFERENCES catalog.car(id_car)
+
+-- CHECK Constraints
+CONSTRAINT CK_CarConcessionAssignment_ValidDateRange CHECK(end_date IS NULL OR start_date<end_date)
   GO
   );
