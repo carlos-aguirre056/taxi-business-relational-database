e13b1cf83f230036032ab6ed3f1c19c5eebd7e0c
carlos-aguirre056 <carlosaguirreo056@gmail.com>
2026-04-30 15:19:58 -0600

    Refactor CarConcessionAssignment table structure

--- 01_ddl/assignment/CarConcessionAssignment.sql
+++ 01_ddl/assignment/CarConcessionAssignment.sql
@@ -1,17 +1,20 @@
 --RELATIONSHIP ATTRIBUTE
   -- 1.2.3 CAR_CONCESSION_ASSIGNMENT
 CREATE TABLE CarConcessionAssignment(
-  id_car_concession_history INT IDENTITY
-, id_car INT
-, id_concession INT
+  id_car_concession_assignment INT IDENTITY
+, id_car INT NOT NULL
+, id_concession INT NOT NULL
 , start_date DATE NOT NULL
 , end_date DATE NULL
 
-  --PK and FK
-CONSTRAINT pk_car_concession_assignment PRIMARY KEY(Id_car_concession_history)
-, CONSTRAINT fk_car_concession_assignment_id_car FOREIGN KEY (id_car) REFERENCES catalog.car(id_car)
 
--- CHECK Constraints
-CONSTRAINT CK_CarConcessionAssignment_ValidDateRange CHECK(end_date IS NULL OR start_date<end_date)
-  GO
+CONSTRAINT PK_CarConcessionAssignment
+    PRIMARY KEY(id_car_concession_assignment)
+, CONSTRAINT FK_CarConcessionAssignment_Car  
+    FOREIGN KEY (id_car) REFERENCES catalog.car(id_car)
+, CONSTRAINT FK_CarConcessionAssignment_Concession
+    FOREIGN KEY (id_concession) REFERENCES catalog.concession(id_concession)
+, CONSTRAINT CK_CarConcessionAssignment_ValidDateRange 
+    CHECK(end_date IS NULL OR start_date<end_date)
   );
+GO
