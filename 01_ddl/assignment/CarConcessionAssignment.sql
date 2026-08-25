0c8c4235fee4ff4ca4ed295d6812ff78b7b6995c
CarlosAguirre_056 <carlosaguirreo056@gmail.com>
2026-08-25 15:43:33 -0600

    DATE -> DATETIME2. Rename DATE columns
    Same modifications as CarDriverAssignment
    
    DATE -> DATETIME2(0)
    
    And rename start_date and end_date to start_at and end_at

--- 01_ddl/assignment/CarConcessionAssignment.sql
+++ 01_ddl/assignment/CarConcessionAssignment.sql
@@ -1,22 +1,23 @@
---RELATIONSHIP ATTRIBUTE
+--RELATIONSHIP ENTITY
   -- 1.2.3 CAR_CONCESSION_ASSIGNMENT
 CREATE TABLE CarConcessionAssignment(
   id_CarConcessionAssignment INT IDENTITY
 , id_car INT NOT NULL
 , id_concession INT NOT NULL
-, start_date DATE NOT NULL
-, end_date DATE NULL
+, start_at DATETIME2(0) NOT NULL
+, end_at DATETIME2(0) NULL
 
 
-CONSTRAINT pk_CarConcessionAssignment PRIMARY KEY(id_CarConcessionAssignment)
+
+, CONSTRAINT pk_CarConcessionAssignment PRIMARY KEY(id_CarConcessionAssignment)
 , CONSTRAINT fk_CarConcessionAssignment_Car FOREIGN KEY (id_car) REFERENCES catalog.car(id_car)
 , CONSTRAINT fk_CarConcessionAssignment_Concession FOREIGN KEY (id_concession) REFERENCES catalog.concession(id_concession)
-, CONSTRAINT ck_CarConcessionAssignment_ValidDateRange CHECK(end_date IS NULL OR start_date<end_date)
+, CONSTRAINT ck_CarConcessionAssignment_ValidDateRange CHECK(end_at IS NULL OR start_at<end_at)
   );
 GO
 
 
 --CREATION OF UNIQUE INDEXES TO ENSURE ONLY ACTIVE ROW PER TABLE
-CREATE UNIQUE INDEX UX_CarConcessionAssignment_ActiveConcession ON CarConcessionAssignment(id_concession) WHERE end_date IS NULL;
+CREATE UNIQUE INDEX UX_CarConcessionAssignment_ActiveConcession ON CarConcessionAssignment(id_concession) WHERE end_at IS NULL;
 
-CREATE UNIQUE INDEX UX_CarConcessionAssignment_ActiveCar ON CarConcessionAssignment(id_car) WHERE end_date IS NULL;
+CREATE UNIQUE INDEX UX_CarConcessionAssignment_ActiveCar ON CarConcessionAssignment(id_car) WHERE end_at IS NULL;
