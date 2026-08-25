48d08a13e370c9c8eb4d1ae90bc94a3784bc5587
CarlosAguirre_056 <carlosaguirreo056@gmail.com>
2026-08-25 15:37:59 -0600

    Add DATETIME2(0) to DATE columns.
    Change of name from start_date and end_date to start_at and end_ at to reflect better the meaning of the column in comparison to the real world business imlication

--- 01_ddl/assignment/CarDriverAssignment.sql
+++ 01_ddl/assignment/CarDriverAssignment.sql
@@ -1,22 +1,22 @@
 --RELATIONSHIP ENTITY
 -- 1.2.2 CAR DRIVER ASSGINMENT
 CREATE TABLE CarDriverAssignment(
-    id_CarDriverHistory INT IDENTITY
+    id_CarDriverAssignment INT IDENTITY
     , id_person INT NOT NULL
     , id_car INT NOT NULL
-    , start_date DATE NOT NULL
-    , end_date DATE NULL
+    , start_at DATETIME(0) NOT NULL
+    , end_at DATETIME(0) NULL
 
-CONSTRAINT pk_CarDriverAssignment PRIMARY KEY(id_CarDriverHistory)
+, CONSTRAINT pk_CarDriverAssignment PRIMARY KEY(id_CarDriverAssignment)
 , CONSTRAINT fk_CarDriverAssignment_PersonIdDriver FOREIGN KEY(id_person) REFERENCES catalog.person(id_person)
 , CONSTRAINT fk_CarDriverAssignment_car_id FOREIGN KEY(id_car) REFERENCES catalog.car(id_car)
-, CONSTRAINT ck_CarDriverAssignment_ValidateRange CHECK(end_date IS NULL OR start_date<end_date)
+, CONSTRAINT ck_CarDriverAssignment_ValidateRange CHECK(end_at IS NULL OR start_at<end_at)
 
-GO);
-
+);
+GO
 
 --CREATION OF UNIQUE INDEXES TO ENSURE THAT THERE IS ONLY ONCE ACTIVE ROW
 
-CREATE UNIQUE INDEX UX_CarDriverAssignment_ActiveCar ON CarDriverAssignment(id_car) WHERE end_date IS NULL;
+CREATE UNIQUE INDEX UX_CarDriverAssignment_ActiveCar ON CarDriverAssignment(id_car) WHERE start_at IS NULL;
 
-CREATE UNIQUE INDEX UX_CarDriverAssignment_ActiveDriver ON CarDriverAssignment(id_person) WHERE end_date IS NULL;
+CREATE UNIQUE INDEX UX_CarDriverAssignment_ActiveDriver ON CarDriverAssignment(id_person) WHERE end_at IS NULL;
