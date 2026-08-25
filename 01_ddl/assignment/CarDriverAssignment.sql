e49a2244aa7d29c5e982d46e52152cd0d415eae2
CarlosAguirre_056 <carlosaguirreo056@gmail.com>
2026-08-25 15:03:17 -0600

    Creation of UNIQUE INDEX in CarDriverAssignment

--- 01_ddl/assignment/CarDriverAssignment.sql
+++ 01_ddl/assignment/CarDriverAssignment.sql
@@ -1,15 +1,22 @@
 --RELATIONSHIP ENTITY
 -- 1.2.2 CAR DRIVER ASSGINMENT
 CREATE TABLE CarDriverAssignment(
-    id_car_driver_history INT IDENTITY
+    id_CarDriverHistory INT IDENTITY
     , id_person INT NOT NULL
     , id_car INT NOT NULL
     , start_date DATE NOT NULL
     , end_date DATE NULL
 
-CONSTRAINT pk_CarDriverAssignment PRIMARY KEY(id_car_driver_history)
-, CONSTRAINT fk_CarDriverAssignment_person_id_driver FOREIGN KEY(id_person) REFERENCES catalog.person(id_person)
+CONSTRAINT pk_CarDriverAssignment PRIMARY KEY(id_CarDriverHistory)
+, CONSTRAINT fk_CarDriverAssignment_PersonIdDriver FOREIGN KEY(id_person) REFERENCES catalog.person(id_person)
 , CONSTRAINT fk_CarDriverAssignment_car_id FOREIGN KEY(id_car) REFERENCES catalog.car(id_car)
 , CONSTRAINT ck_CarDriverAssignment_ValidateRange CHECK(end_date IS NULL OR start_date<end_date)
 
 GO);
+
+
+--CREATION OF UNIQUE INDEXES TO ENSURE THAT THERE IS ONLY ONCE ACTIVE ROW
+
+CREATE UNIQUE INDEX UX_CarDriverAssignment_ActiveCar ON CarDriverAssignment(id_car) WHERE end_date IS NULL;
+
+CREATE UNIQUE INDEX UX_CarDriverAssignment_ActiveDriver ON CarDriverAssignment(id_person) WHERE end_date IS NULL;
