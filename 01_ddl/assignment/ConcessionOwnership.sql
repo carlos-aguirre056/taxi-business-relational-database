b65270f7b581ff7e30c9bcefc115be9704efab21
CarlosAguirre_056 <carlosaguirreo056@gmail.com>
2026-08-25 13:31:37 -0600

    Fix primary key reference in ConcessionOwnership table

--- 01_ddl/assignment/ConcessionOwnership.sql
+++ 01_ddl/assignment/ConcessionOwnership.sql
@@ -2,13 +2,13 @@
 --1.2.1 Concession Ownership
 
 CREATE TABLE ConcessionOwnership(
-  , id_ownership INT IDENTITY
+  id_ownership INT IDENTITY
   , id_concession INT UNIQUE NOT NULL
   , id_person INT UNIQUE NOT NULL
   , start_date DATE NOT NULL
   , end_date DATE NULL
 
-CONSTRAINT pk_ConcessionOwnership PRIMARY KEY(id_owner)
+CONSTRAINT pk_ConcessionOwnership PRIMARY KEY(id_ownership)
 , CONSTRAINT fk_ConcessionOnwership_ConcessionFK FOREIGN KEY(id_concession) REFERENCES concession
 , CONSTRAINT fk_ConcessionOwnership_PersonFK FOREIGN KEY(id_person) REFERENCES person
 , CONSTRAINT ck_ConcessionOwnership_ValidDateRange CHECK(end_date IS  NULL OR start_date<end_date)
