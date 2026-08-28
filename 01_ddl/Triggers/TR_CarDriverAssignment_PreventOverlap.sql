59a6474fe76a020786b7d60b8a8ca0705eff016f
CarlosAguirre_056 <carlosaguirreo056@gmail.com>
2026-08-27 23:51:56 -0600

    Fix correct name of id of CarConcessionAssignment

--- 01_ddl/Triggers/TR_CarDriverAssignment_PreventOverlap.sql
+++ 01_ddl/Triggers/TR_CarDriverAssignment_PreventOverlap.sql
@@ -7,35 +7,37 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Prevent overlapping assignments for the same car
+    -- Prevent overlapping inserts for the same car
     IF EXISTS
     (
         SELECT 1
         FROM inserted AS i
         INNER JOIN CarDriverAssignment AS c
             ON c.id_car = i.id_car
-           AND c.id_car_driver_assignment <> i.id_car_driver_assignment
+           AND c.id_CarDriverAssignment <> i.id_car_driver_assignment
            AND i.start_date < COALESCE(c.end_date, '9999-12-31 23:59:59')
            AND c.start_date < COALESCE(i.end_date, '9999-12-31 23:59:59')
     )
     BEGIN
-        THROW 50001 --custom error number assigned to car overlap ,'Temporal integrity violation: overlapping driver assignments exist for the same car.', 1;
+        THROW 50001 --custom error number assigned to car overlap 
+        ,'Temporal integrity violation: overlapping driver assignments exist for the same car.', 1;
     END;
 
 
-    -- Prevent overlapping assignments for the same driver
+    -- Prevent overlapping inserts for the same driver
     IF EXISTS
     (
         SELECT 1
         FROM inserted AS i
         INNER JOIN CarDriverAssignment AS c
             ON c.id_person = i.id_person
-           AND c.id_car_driver_assignment <> i.id_car_driver_assignment
+           AND c.id_CarDriverAssignment <> i.id_car_driver_assignment
            AND i.start_date < COALESCE(c.end_date, '9999-12-31 23:59:59')
            AND c.start_date < COALESCE(i.end_date, '9999-12-31 23:59:59')
     )
     BEGIN
-        THROW 50002 --custom error number assigned to car overlap , 'Temporal integrity violation: overlapping car assignments exist for the same driver.', 1;
+        THROW 50002 --custom error number assigned to driver overlap
+        ,'Temporal integrity violation: overlapping car assignments exist for the same driver.', 1;
     END;
 END;
 GO
