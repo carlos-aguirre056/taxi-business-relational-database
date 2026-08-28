e88c0c27be58d837e7f1a400162d32f61ef93e1f
CarlosAguirre_056 <carlosaguirreo056@gmail.com>
2026-08-28 00:21:57 -0600

    Rename table from concessions to concession

--- 01_ddl/catalog/Create_Concession.sql
+++ 01_ddl/catalog/Create_Concession.sql
@@ -1,6 +1,6 @@
 ---1.1 CORE ENTITY
 --- 1.1.1 CONCESSION
-CREATE TABLE catalog.concessions(
+CREATE TABLE catalog.concession(
 	  id_concession INT IDENTITY(1,1),
 	  economic_number VARCHAR(10) UNIQUE NOT NULL,
 	  authorization_number VARCHAR(10) UNIQUE NOT NULL,
