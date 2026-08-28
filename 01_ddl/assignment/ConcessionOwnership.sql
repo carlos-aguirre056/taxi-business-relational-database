2dd5e06c0c78803cba8b17dfeb3648a984cd97af
CarlosAguirre_056 <carlosaguirreo056@gmail.com>
2026-08-28 00:45:33 -0600

    Refactor ConcessionOwnership table and constraints

--- 01_ddl/assignment/ConcessionOwnership.sql
+++ 01_ddl/assignment/ConcessionOwnership.sql
@@ -1,21 +1,22 @@
--- 1.2 RELATIONSHIP ENTITY
+-- 1.2 RELATIONSHIP ENTITIES
 --1.2.1 Concession Ownership
 
-CREATE TABLE ConcessionOwnership(
-  id_ownership INT IDENTITY
+CREATE TABLE assignment.ConcessionOwnership
+(
+  id_ownership INT IDENTITY(1,1) NOT NULL
   , id_concession INT NOT NULL
   , id_person INT NOT NULL
-  , start_date DATE NOT NULL
-  , end_date DATE NULL
+  , start_at DATETIME2(0) NOT NULL
+  , end_at DATETIME2(0) NULL
 
-CONSTRAINT pk_ConcessionOwnership PRIMARY KEY(id_ownership)
-, CONSTRAINT fk_ConcessionOwnership_ConcessionFK FOREIGN KEY(id_concession) REFERENCES catalog.concession
-, CONSTRAINT fk_ConcessionOwnership_PersonFK FOREIGN KEY(id_person) REFERENCES catalog.person
-, CONSTRAINT ck_ConcessionOwnership_ValidDateRange CHECK(end_date IS  NULL OR start_date<end_date)
-  GO
+, CONSTRAINT pk_ConcessionOwnership PRIMARY KEY(id_ownership)
+, CONSTRAINT fk_ConcessionOwnership_ConcessionFK FOREIGN KEY(id_concession) REFERENCES catalog.concession(id_concession)
+, CONSTRAINT fk_ConcessionOwnership_PersonFK FOREIGN KEY(id_person) REFERENCES catalog.person(id_person)
+, CONSTRAINT ck_ConcessionOwnership_ValidDateRange CHECK(end_at IS  NULL OR start_at<end_at)
   );
-
+GO
 
 --CREATION OF UNIQUE INDEX TO ENSURE ONLY ONE ACTIVE ROW
 
-CREATE UNIQUE INDEX UX_ConcessionOwnership_ActiveConcession ON ConcessionOwnership(id_concession) WHERE end_date IS NULL;
+CREATE UNIQUE INDEX UX_ConcessionOwnership_ActiveConcession ON assignment.ConcessionOwnership(id_concession) WHERE end_date IS NULL;
+GO
