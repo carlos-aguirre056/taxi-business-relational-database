4e8b1c185e62ed258f8e8731458fb8a00d05d530
CarlosAguirre_056 <carlosaguirreo056@gmail.com>
2026-08-28 00:33:57 -0600

    Modify Create_Concession.sql to add NOT NULL constraint
    Added NOT NULL constraint to id_concession column.

--- 01_ddl/catalog/Create_Concession.sql
+++ 01_ddl/catalog/Create_Concession.sql
@@ -1,10 +1,12 @@
 ---1.1 CORE ENTITY
 --- 1.1.1 CONCESSION
-CREATE TABLE catalog.concession(
-	  id_concession INT IDENTITY(1,1),
-	  economic_number VARCHAR(10) UNIQUE NOT NULL,
-	  authorization_number VARCHAR(10) UNIQUE NOT NULL,
-
-CONSTRAINT pk_concession PRIMARY KEY(id_concession)
+CREATE TABLE catalog.concession
+(
+	  id_concession INT IDENTITY(1,1) NOT NULL
+	, economic_number VARCHAR(10) UNIQUE NOT NULL
+	, authorization_number VARCHAR(10) UNIQUE NOT NULL
+	
+	
+	, CONSTRAINT pk_concession PRIMARY KEY(id_concession)
 );
 GO
